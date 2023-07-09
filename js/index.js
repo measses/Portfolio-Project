ad4bafc292046fe4fce43ea0baccfd630b65231b
Mert Araz <mertoreyiz1907@gmail.com>
2023-07-09 23:15:02 +0300

    responsive finished

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
-//scroll sections
 
+//toggle menu/navbar script
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x'); 
+  navbar.classList.toggle('active');
+}
+
+
+
+
+
+//scroll sections
+let header = document.querySelector("header");
+let sections = document.querySelectorAll("section");
+let navLinks= document.querySelectorAll("header nav a");
 window.onscroll = () => {
-    let header = document.querySelector("header");
+
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    }
+  })
+  
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x'); 
+    navbar.classList.remove('active');
 }
 
 document.addEventListener('click', function(event) {
@@ -17,3 +50,4 @@ document.addEventListener('click', function(event) {
       cursorEffect.remove();
     }, 200);
   });
+
