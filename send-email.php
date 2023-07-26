6a3f2b97cd57901491d86c83dc1fc31aa48586ef
Mert Araz <mertoreyiz1907@gmail.com>
2023-07-26 15:11:58 +0300

    email function finished

--- send-email.php
+++ send-email.php
@@ -1,33 +1,45 @@
 <?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$about = $_POST['about'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-require "vendor/autoload.php";
-
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP; 
-$mail = new PHPMailer(true);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require 'vendor/autoload.php'; // PHPMailer'ın dahil olduğu composer autoload dosyasını dahil edin
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
+    $name = $_POST['name'];
+    $email = $_POST['mail'];
+    $phone = $_POST['phone'];
+    $about = $_POST['about'];
+    $message = $_POST['message'];
 
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+    echo smtp_mailer('mertoreyiz1907@gmail.com', $about, "Ad Soyad: $name<br>E-Mail: $email<br>Telefon: $phone<br>Mesaj: $message");
+}
 
-$mail->Username = 'you@gmail.com';
-$mail->Password = 'password';
+function smtp_mailer($to, $subject, $msg)
+{
+    require 'vendor/phpmailer/phpmailer/src/PHPMailerAutoload.php'; // PHPMailer sınıfını dahil et
 
-$mail->setFrom($email, $name);
-$mail->addAddress("mertaraz7@gmail.com", "Mert");
-
-$mail->Subject = $about;
-$mail->Body = $message;
-
-$mail->send();
-echo "Mail sent";
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 587;
+    $mail->IsHTML(true);
+    $mail->CharSet = 'UTF-8';
+    //$mail->SMTPDebug = 2; 
+    $mail->Username = "mertarazyedek23@gmail.com";
+    $mail->Password = "pwekiikavtksxelc";
+    $mail->setFrom('mertarazyedek23@gmail.com', 'Mert Araz');
+    $mail->Subject = $subject;
+    $mail->Body = $msg;
+    $mail->AddAddress($to);
+    $mail->SMTPOptions = array('ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => false
+    ));
+    if (!$mail->Send()) {
+        echo $mail->ErrorInfo;
+    } else {
+        return 'Sent';
+    }
+}
 ?>
