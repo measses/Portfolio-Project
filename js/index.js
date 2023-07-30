c896f0207f46d1e01eab1458d06d14592a0533ef
Mert Araz <mertoreyiz1907@gmail.com>
2023-07-31 01:54:55 +0300

    email js

--- js/index.js
+++ js/index.js
@@ -106,4 +106,23 @@ window.onload = function() {
 }
 
 
+const btn = document.querySelector('.btn');
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  btn.textContent = 'Sending...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_uo9fxwu';
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.textContent = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.textContent = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
 
